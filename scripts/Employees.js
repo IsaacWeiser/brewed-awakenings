1da2db678e12ccc570cb94ca2e97dd92c858c62b
Isaac Weiser <isaac.weiser.x@gmail.com>
2021-11-29 22:32:00 -0500

    added event listener to employee list

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,4 +1,7 @@
 import { getEmployees } from "./database.js"
+import { getOrders } from "./database.js"
+
+const orders = getOrders();
 
 const employees = getEmployees()
 
@@ -14,3 +17,37 @@ export const Employees = () => {
     return html
 }
 
+//add a click listener that lists how many products that employee sold
+document.addEventListener("click", (event)=> {
+
+    //identify what is being clicked and make sure that we are clicking employees
+    if (event.target.id.startsWith("employee--") ) {
+
+        // now that we have specified the areas of click
+        //got through the employee data and show the  amount of products sold by the employee
+        
+        //this line splits the id into two parts, the word employee and the id of db item
+        // we are capturing the second index aka [1] in this variable 
+        let [,empId] = event.target.id.split("--");
+let count =0;
+console.log()
+        for (const order of orders) {
+            //match the employees to the orders and count products sold
+            if (parseInt(empId) === order.employeeId) {
+            console.log("test")
+            count++;
+            console.log(count)
+            }
+        }
+let empName;
+        for (const employee of employees) {
+            if (parseInt(empId)=== employee.id)
+            {
+                empName=employee.name;
+            }  
+        }
+        window.alert(`
+        ${empName} has sold:
+        ${count} products`)
+    }
+})
